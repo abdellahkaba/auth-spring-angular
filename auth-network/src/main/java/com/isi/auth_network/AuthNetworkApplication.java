907c13ae60548009237c3982fcd5cb762410c3bf
Abdellah <abdallahkaba98@gmail.com>
2024-08-05 00:12:17 +0000

    first-commit

---
+++ auth-network/src/main/java/com/isi/auth_network/AuthNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.isi.auth_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthNetworkApplication.class, args);
+	}
+
+}

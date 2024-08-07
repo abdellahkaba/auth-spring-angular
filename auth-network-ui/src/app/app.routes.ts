a466c622886a120a0075b80a7ba4f89508ccd3f6
Abdellah <abdallahkaba98@gmail.com>
2024-08-07 10:52:11 +0000

    init-to-implement-model-student

--- auth-network-ui/src/app/app.routes.ts
+++ auth-network-ui/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
 import {RegisterComponent} from "./pages/register/register.component";
 import {ActivateAccountComponent} from "./pages/activate-account/activate-account.component";
+import {StudentComponent} from "./pages/student/student.component";
 
 export const routes: Routes = [
   {
@@ -16,5 +17,9 @@ export const routes: Routes = [
     path: 'activate-account',
     component: ActivateAccountComponent
   },
+  {
+    path: 'student',
+    component: StudentComponent
+  }
 
 ];
